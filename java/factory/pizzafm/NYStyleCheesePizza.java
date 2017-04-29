9698a70942fe4c04376214513195560de8b742b1
Chris Schalago <chrisschalago@gmail.com>
2017-04-29 16:39:26 -0400

    Add pizza example of Factory Method Pattern

---
+++ java/factory/pizzafm/NYStyleCheesePizza.java
@@ -0,0 +1,10 @@
+public class NYStyleCheesePizza extends Pizza {
+
+	public NYStyleCheesePizza() { 
+		name = "NY Style Sauce and Cheese Pizza";
+		dough = "Thin Crust Dough";
+		sauce = "Marinara Sauce";
+ 
+		toppings.add("Grated Reggiano Cheese");
+	}
+}
