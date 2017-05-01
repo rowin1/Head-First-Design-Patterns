390b04d4f2e9cdc5ef2f7511a500689141a7018b
Chris Schalago <chrisschalago@gmail.com>
2017-05-01 17:40:20 -0400

    Add 'Undo button' example of Command Pattern

---
+++ java/command/undo/LightOffCommand.java
@@ -0,0 +1,17 @@
+public class LightOffCommand implements Command {
+	Light light;
+	int level;
+	
+  public LightOffCommand(Light light) {
+		this.light = light;
+	}
+ 
+	public void execute() {
+    level = light.getLevel();
+		light.off();
+	}
+ 
+	public void undo() {
+		light.dim(level);
+	}
+}
