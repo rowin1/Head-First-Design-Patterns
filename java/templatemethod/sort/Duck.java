e5b02c82f8f7fe3141c52dfe6b91d5be0f8c3039
Chris Schalago <chrisschalago@gmail.com>
2017-05-03 09:33:12 -0400

    Add 'compareTo' example of Template Method Pattern with sort

---
+++ java/templatemethod/sort/Duck.java
@@ -0,0 +1,27 @@
+public class Duck implements Comparable {
+  String name;
+  int weight;
+  
+  public Duck(String name, int weight) {
+    this.name = name;
+    this.weight = weight;
+  }
+  
+  public String toString() {
+		return name + " weighs " + weight;
+	}
+  
+  public int compareTo(Object object) {
+    Duck otherDuck = (Duck)object;
+    
+    if (this.weight < otherDuck.weight) {
+			return -1;
+		} 
+    else if (this.weight == otherDuck.weight) {
+			return 0;
+		} 
+    else { 
+			return 1;
+		}
+  }
+}
