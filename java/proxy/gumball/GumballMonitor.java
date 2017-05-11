7e7bd4583d3806422a4313e952c778fe1bf211cf
Chris Schalago <chrisschalago@gmail.com>
2017-05-11 10:17:50 -0400

    Add Gumball Machine example of Proxy Pattern

---
+++ java/proxy/gumball/GumballMonitor.java
@@ -0,0 +1,20 @@
+import java.rmi.*;
+
+public class GumballMonitor {
+  GumballMachineRemote machine;
+  
+  public GumballMonitor(GumballMachineRemote machine) {
+    this.machine = machine;
+  }
+  
+  public void report() {
+    try {
+      System.out.println("Gumball Machine: " + machine.getLocation());
+      System.out.println("Current inventory: " + machine.getCount() + "gumballs");
+      System.out.println("Current state: " + machine.getState());
+    }
+    catch (RemoteException  e) {
+      e.printStackTrace();
+    }
+  }
+}
